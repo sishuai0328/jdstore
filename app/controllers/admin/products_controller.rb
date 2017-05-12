ea5c1673311218b1f7938117bc77ebe1eb0b6cbd
sish <sishuai0328@gmail.com>
2017-05-12 16:16:03 +0800

    change admin/product backend panel

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -41,6 +41,7 @@ class Admin::ProductsController < ApplicationController
   end
 
 
+
   private
 
   def product_params
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   resources :products
 
-  root 'product#index'
+  root 'products#index'
 end
