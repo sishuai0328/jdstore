577faf52deb1bf4a352fd1c6d5976ceb64938d60
sish <sishuai0328@gmail.com>
2017-05-14 20:50:29 +0800

    cannot add same item in cart

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,13 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
+
+  resources :cart_items
 
   root 'products#index'
 end
