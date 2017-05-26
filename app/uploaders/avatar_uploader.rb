99782f17c802e47590de5dfef4aa2b1678e05d73
sish <sishuai0328@gmail.com>
2017-05-27 02:48:46 +0800

    update products index

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -21,6 +21,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [200,200]
   end
 
+  version :small do
+    process resize_to_fill: [50,50]
+  end
+
   version :medium do
     process resize_to_fill: [400,400]
   end
