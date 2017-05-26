38989bd4fb8f69f199f503e3eecbd489b8d33a61
GongJS <382663074@qq.com>
2017-05-26 14:38:18 +0800

    change the navbar and product show image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,13 +14,27 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require jquery
-//= require bootstrap/alert
-//= require bootstrap/dropdown
-//= require bootstrap-sprockets
+//= require bootstrap
+
 //= require_tree .
 
-$(document).on('mouseover', '.productDetail-left-imageList-item', function () {
-  var src = $(this).find('img').attr('src') //从被鼠标选中的图片的src里面拿到图片链接
-  $('.productDetail-left-bigImage').find('img').attr('src', src) //把图片链接设置到大图的src里面
+/*===== Welcome#index - 首頁導航列變化 =====*/
+$(window).scroll(function () {
+	var $navbar = $('#navbar')
+	if ($(this).scrollTop() > 125) {
+		$('#navbar').addClass('show_bgcolor')
+	} else {
+		$('#navbar').removeClass('show_bgcolor')
+	}
+})
+
+/*===== Products#show - 變更顯示圖片 =====*/
+$(document).on('mouseover', '.list-image', function () {
+  var src_other = $(this).attr('src') //抓取小圖圖片路徑
+  var src_main = src_other.toString().replace("other", "main") //更改圖片路徑
+
+  $('.main-image').attr('src', src_main) //變更大圖圖片路徑
+
+  $('.list-image').removeClass('list-image-active') //其他圖片移除鎖定狀態
+  $(this).addClass('list-image-active') //當前圖片新增鎖定狀態
 })
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,16 +15,16 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [400, 400]
 
   version :small do
     process resize_to_fit: [100, 100]
   end
 
-  version :thumb do
-    process resize_to_fit: [200, 200]
-  end
 
+  version :other do
+    process resize_to_fill: [200, 200]
+  end
   version :medium do
     process resize_to_fit: [400, 400]
   end
