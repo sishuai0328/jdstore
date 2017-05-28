dc6803b4a4c2a2db3a184da9ee5b410f1a8a70b5
GongJS <382663074@qq.com>
2017-05-28 16:55:34 +0800

    solve the heroku bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,24 @@
 //= require bootstrap
 
 //= require_tree .
+
+/*===== Welcome#index - 首頁導航列變化 =====*/
+$(window).scroll(function () {
+	var $navbar = $('#navbar')
+	if ($(this).scrollTop() > 125) {
+		$('#navbar').addClass('show_bgcolor')
+	} else {
+		$('#navbar').removeClass('show_bgcolor')
+	}
+})
+
+/*===== Products#show - 變更顯示圖片 =====*/
+$(document).on('mouseover', '.list-image', function () {
+  var src_other = $(this).attr('src') //抓取小圖圖片路徑
+  var src_main = src_other.toString().replace("other", "main") //更改圖片路徑
+
+  $('.main-image').attr('src', src_main) //變更大圖圖片路徑
+
+  $('.list-image').removeClass('list-image-active') //其他圖片移除鎖定狀態
+  $(this).addClass('list-image-active') //當前圖片新增鎖定狀態
+})
